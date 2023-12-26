6b8798b2230fd408f581bcb4f01492255739124a
Andris Spalvis <spalvis.andris@gmail.com>
2023-12-26 12:25:57 +0200

    OrderController + OrderListVM

---
+++ Models/ViewModels/OrderListVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Models.ViewModels
+{
+    public class OrderListVM
+    {
+        public IEnumerable<OrderHeader> OrderHeaders { get; set; }
+
+        public IEnumerable<SelectListItem> StatusList { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ Webshop.Mvc/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models.ViewModels;
+using Utility.BrainTree;
+
+namespace Webshop.Mvc.Controllers
+{
+    public class OrderController : BaseController
+    {
+        private readonly IOrderDetailsRepository _orderDetailsRepository;
+        private readonly IOrderHeaderRepository _orderHeaderRepository;
+        private readonly IBrainTreeGate _brainTreeGate;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
+
+        public OrderController(
+            IOrderDetailsRepository orderDetailsRepository,
+            IOrderHeaderRepository orderHeaderRepository,
+            IBrainTreeGate brainTreeGate)
+        {
+            _orderDetailsRepository = orderDetailsRepository;
+            _orderHeaderRepository = orderHeaderRepository;
+            _brainTreeGate = brainTreeGate;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
